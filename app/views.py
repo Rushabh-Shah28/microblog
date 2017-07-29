d9f05cf5efbc7486788038b920a2092a19a58fd8
Rushabh Shah <rshah@gocatalant.com>
2017-07-29 17:10:36 -0400

    Part 2 before inheritance
    Basic conditional statements and for loops

--- app/views.py
+++ app/views.py
@@ -1,7 +1,22 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 
 def index():
-    return "Hello Flask!"
+    user = {'name': 'Rushabh'}
+    posts = [
+        {
+            'author': "J.K Rowling",
+            'description': 'Beautiful day in Portland!'
+        },
+        {
+            'author': 'Barack Obama',
+            'description': 'The Avengers movie was so cool!'
+        }
+    ]
+    return render_template("index.html",
+                           title='Home',
+                           user=user,
+                           posts=posts)
